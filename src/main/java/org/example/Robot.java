8d4e7ba844d8b07a85fc502c1bbc34c44f98233a
User <gicagarcia58@gmail.com>
2024-04-11 18:32:58 -0300

    feat: implement colisionPoint to make test pass

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.function.IntPredicate;
+
+public class Robot {
+
+    public Object colisionPoint(int x1, int x2, int a, int f, int d){
+        if(d > 8){
+            return "Error";
+        }
+        return -1;
+    }
+
+}
--- src/test/java/org/example/RobotTest.java
+++ src/test/java/org/example/RobotTest.java
@@ -6,9 +6,11 @@ import static org.assertj.core.api.Assertions.*;
 public class RobotTest {
 
     @Test
-    void shouldNotAcceptDistanceOverLimit(){
+    public void shouldNotAcceptDistanceOverLimit(){
         Robot robot = new Robot();
-        assertThat(robot.colisionPoint(10, 10, 45, 45, 10)).isEqualTo("Error");
+        assertThat(robot.colisionPoint(10, 10, 45, 45, 10))
+                .isInstanceOf(String.class)
+                .isEqualTo("Error");
     }
 
 }
