dd883a999446fcf619f63caa6bf09a5688fd8ef1
User <gicagarcia58@gmail.com>
2024-04-11 18:53:33 -0300

    refactor(test): add setUp before each test

--- src/test/java/org/example/RobotTest.java
+++ src/test/java/org/example/RobotTest.java
@@ -1,20 +1,27 @@
 package org.example;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class RobotTest {
 
+    Robot robot;
+
+    @BeforeEach
+    void setUp(){
+        robot = new Robot();
+    }
+
     @Test
     public void shouldNotAcceptDistanceOverLimit(){
-        Robot robot = new Robot();
         assertThat(robot.colisionPoint(10, 10, 45, 45, 10))
                 .isEqualTo("Error");
     }
 
     @Test
     public void shouldNotAcceptNegativeDistance(){
-        Robot robot = new Robot();
         assertThat(robot.colisionPoint(1, 1, 45, 60, -4))
                 .isEqualTo("Error");
     }
