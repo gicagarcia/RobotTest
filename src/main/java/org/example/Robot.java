b162ea0b82adfa651218cabcffd8de80c555c697
User <gicagarcia58@gmail.com>
2024-04-11 18:40:22 -0300

    refactor: error to negative distance

--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -5,7 +5,7 @@ import java.util.function.IntPredicate;
 public class Robot {
 
     public Object colisionPoint(int x1, int x2, int a, int f, int d){
-        if(d > 8){
+        if(d > 8 || d < 0){
             return "Error";
         }
         return -1;
