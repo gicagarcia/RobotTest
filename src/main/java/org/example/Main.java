b1d8fcd7d9d014778f4f8cd65fd455e4b6135637
User <gicagarcia58@gmail.com>
2024-04-12 17:17:06 -0300

    feat: implemented main to run program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Robot robot = new Robot();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inform position X:");
+        Integer x1 = scanner.nextInt();
+
+        System.out.println("Inform position Y:");
+        Integer y1 = scanner.nextInt();
+
+        System.out.println("Inform angle:");
+        Integer a = scanner.nextInt();
+
+        System.out.println("Inform beam:");
+        Integer f = scanner.nextInt();
+
+        System.out.println("Inform obstacle distance:");
+        Integer d = scanner.nextInt();
+
+        ColisionResult result = robot.colisionPoint(x1, y1, a, f, d);
+        if(result.hasError()){
+            String error = result.getErrorMessage();
+            System.out.println(error);
+        }else{
+            Integer[] coordinates = result.getCoordinates();
+            String colision = String.format("Colision point: (%d, %d)", coordinates[0], coordinates[1]);
+            System.out.println(colision);
+        }
     }
 }
