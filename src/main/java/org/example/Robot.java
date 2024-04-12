b5a5c310679648e1292d149e83e0406d9f285e31
User <gicagarcia58@gmail.com>
2024-04-12 15:58:45 -0300

    feat: implemented calculateColisionPoint func and refactor to use new class

--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -4,15 +4,33 @@ import java.util.function.IntPredicate;
 
 public class Robot {
 
-    public Object colisionPoint(Integer x1, Integer x2, Integer a, Integer f, Integer d){
+    public ColisionResult colisionPoint(Integer x1, Integer x2, Integer a, Integer f, Integer d){
         if(d > 8 || d < 0){
-            return "Error";
+            return new ColisionResult("Error");
         }
-        return new Integer[]{1, 2};
+
+        Integer angle = recalculateAngle(a, f);
+        Integer[] coordinates = calculateColisionPoint(angle, d);
+
+        return new ColisionResult(coordinates);
     }
 
     public Integer recalculateAngle(Integer a, Integer f){
         return (a+f) - 90;
+    }
+
+    private Integer[] calculateColisionPoint(Integer angle, Integer d) {
+        double radians = Math.toRadians(angle);
+        double sin = Math.sin(radians);
+        double cos = Math.cos(radians);
+
+        double x = d * cos;
+        double y = d * sin;
+
+        Integer roundX = (int) Math.round(x);
+        Integer roundY = (int) Math.round(y);
+
+        return new Integer[]{roundX, roundY};
     }
 
 }
